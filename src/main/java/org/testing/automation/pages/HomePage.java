2f92624ae4136850e478ed48ddcc49abe738c161
PiotrBK <kaczmarek.piotr2@gmail.com>
2023-05-05 11:04:31 +0200

    page framework

--- src/main/java/org/testing/automation/pages/HomePage.java
+++ src/main/java/org/testing/automation/pages/HomePage.java
@@ -1,11 +1,28 @@
 package org.testing.automation.pages;
 
+import org.testing.automation.Browser;
 import org.testing.automation.Page;
 
 
 public class HomePage extends Page {
 
-    public String url = "https://learn.microsoft.com/pl-pl/";
-    public String title = "Microsoft Learn: Zdobywanie umiejętności, które otwierają drzwi w Twojej karierze";
+    static String url = "https://learn.microsoft.com/pl-pl/";
+    static String title = "Microsoft Learn: Zdobywanie umiejętności, które otwierają drzwi w Twojej karierze";
+
+
+    @Override
+    public void goTo(){
+        Browser.goTo(url);
+
+    }
+
+    @Override
+    public boolean isAt() {
+        return Browser.title().equals(title);
+
+
+    }
+
+
 
 }
