e78d678017d185550972199e6994344c2a71b950
PiotrBK <kaczmarek.piotr2@gmail.com>
2023-05-16 12:42:29 +0200

    Commit 2

--- src/main/java/org/testing/automation/pages/SearchPage.java
+++ src/main/java/org/testing/automation/pages/SearchPage.java
@@ -1,7 +1,13 @@
 package org.testing.automation.pages;
-
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.devtools.v85.css.CSS;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import javax.xml.xpath.XPath;
+import java.util.List;
 
 public class SearchPage {
 
@@ -11,11 +17,20 @@ public class SearchPage {
     private String title = "Wyszukaj | Microsoft Learn";
 
     private String url;
+    @FindBy(css = "div.margin-top-xxs:nth-child(1)")
+    private List<WebElement> radiobuttons;
+
+    @FindBy(css = "ul.margin-top-sm")
+    private List<WebElement> searchResults;
+
     public SearchPage (WebDriver driver){
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
 
+    public String getURL(){
+        return driver.getCurrentUrl();
+    }
 
     public void goTo(){
         driver.get(url);
@@ -26,8 +41,15 @@ public class SearchPage {
         return driver.getTitle().equals(title);
     }
 
+    public void clickCertyfikaty(){
+        radiobuttons.get(3).click();
 
+    }
 
+    public void clickFirstResult(){
+        WebElement firstResult = searchResults.get(0).findElement(By.cssSelector("li.margin-top-sm:nth-child(1) > h2:nth-child(1))");
+        firstResult.click();
 
+    }
 
 }
