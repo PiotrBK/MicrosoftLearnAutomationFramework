c8340b8da3ca1f91f1e67af6e0401e3c7a34d61e
PiotrBK <kaczmarek.piotr2@gmail.com>
2023-05-16 12:44:04 +0200

    Commit 2

--- src/main/java/org/testing/automation/pages/SearchPage.java
+++ src/main/java/org/testing/automation/pages/SearchPage.java
@@ -2,7 +2,6 @@ package org.testing.automation.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.devtools.v85.css.CSS;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
